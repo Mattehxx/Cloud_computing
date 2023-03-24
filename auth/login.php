923ce476a7fc12e0ead998b9b8e3a9065d0ffe54
Mattehxx <103050128+Mattehxx@users.noreply.github.com>
2023-03-24 15:44:52 +0100

    Changed login.php

--- auth/login.php
+++ auth/login.php
@@ -5,13 +5,12 @@
     require_once('../config/db_connection.php');
 
     //prende i valori dal form e li assegna alle variabili
-    $username = sanitize_string($_POST['username']); // sanitize
-    $password = $_POST['password'];
+    $username=sanitize_string($_POST['username']); // sanitize
+    $password=$_POST['password'];
 
-
-    $prpstm = $conn->prepare("SELECT id, password FROM accounts WHERE email LIKE ? OR username LIKE ?");
-
+    $prpstm=$conn->prepare("SELECT id, password FROM accounts WHERE email LIKE ? OR username LIKE ?");
     $prpstm->bind_param('ss', $username, $username);
+
     if (!$prpstm->execute()) {
         http_response_code(400);
         echo json_encode(['message' => "Non è stato possibile eseguire l'operazione"]);
@@ -19,19 +18,21 @@
 
     $prpstm->bind_result($id, $passworddb);
     $prpstm->fetch();
-    if(!password_verify($password, $passworddb)){
+
+    //se l'hash della password corrisponde
+    if(!password_verify($password, $passworddb)) {
         http_response_code(400);
         echo json_encode(['message' => "Non è stato possibile eseguire l'operazione"]);
     }
 
     //se non è presente l'utente all'interno del db
-    if ($id === NULL) {
+    if ($id===NULL) {
         http_response_code(401);
         echo json_encode(['message' => "Nessun è stato possibile trovare un utente con le credenziali fornite"]);
     }
 
-
-    $_SESSION['idUser'] = $id;
+    //se tutto va bene e le credenziali corrispondono
+    $_SESSION['idUser']=$id;
     http_response_code(400);
     echo json_encode(['message' => "Operazione andata a buon fine"]);
 ?>
