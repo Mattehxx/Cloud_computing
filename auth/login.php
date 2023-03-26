755bb43cfb38bc73f51ba7d5631e8e5a3f495089
Mattehxx <103050128+Mattehxx@users.noreply.github.com>
2023-03-26 19:41:39 +0200

    Added functions/resoponse.php and changed auth/login.php

--- auth/login.php
+++ auth/login.php
@@ -2,6 +2,7 @@
     session_start();
 
     require_once('../functions/sanitize_string.php');
+    require_once('../functions/response.php');
     require_once('../config/db_connection.php');
 
     //prende i valori dal form e li assegna alle variabili
@@ -12,8 +13,7 @@
     $prpstm->bind_param('ss', $username, $username);
 
     if (!$prpstm->execute()) {
-        http_response_code(400);
-        echo json_encode(['message' => "Non è stato possibile eseguire l'operazione"]);
+        bad();
     }
 
     $prpstm->bind_result($id, $passworddb);
@@ -21,18 +21,15 @@
 
     //se l'hash della password corrisponde
     if(!password_verify($password, $passworddb)) {
-        http_response_code(400);
-        echo json_encode(['message' => "Non è stato possibile eseguire l'operazione"]);
+        bad();
     }
 
     //se non è presente l'utente all'interno del db
     if ($id===NULL) {
-        http_response_code(401);
-        echo json_encode(['message' => "Nessun è stato possibile trovare un utente con le credenziali fornite"]);
+        unauthorized();
     }
 
     //se tutto va bene e le credenziali corrispondono
     $_SESSION['idUser']=$id;
-    http_response_code(400);
-    echo json_encode(['message' => "Operazione andata a buon fine"]);
+    ok();
 ?>
